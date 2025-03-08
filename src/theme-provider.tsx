b62d293ba01f0cfaec1cf2e65c437be573d9fe91
Johnny Gérard <john@jgerard.dev>
2025-03-08 06:19:25 +0100

    feat: add theme-provider.tsx

---
+++ src/theme-provider.tsx
@@ -0,0 +1,78 @@
+"use client";
+import { THEME_KEY } from "@/constants.js";
+import { ThemeContext } from "@/theme-context.js";
+import { type Theme, THEME } from "@/type/theme.js";
+import { memo, type ReactNode, useEffect, useRef, useState } from "react";
+
+type Props = {
+  children: ReactNode;
+  initialTheme: Theme;
+};
+
+export const ThemeProvider = memo(({ children, initialTheme }: Props) => {
+  const [theme, setTheme] = useState(initialTheme);
+
+  const channelRef = useRef<BroadcastChannel | undefined>(
+    window.BroadcastChannel && new BroadcastChannel(THEME_KEY),
+  );
+
+  // Listen for theme change from other tabs
+  useEffect(() => {
+    const channel = channelRef.current;
+
+    if (channel === undefined) {
+      console.warn(
+        "Unsupported BroadcastChannel: Unable to synchronize theme across tabs.",
+      );
+      return;
+    }
+
+    channel.onmessage = ({ data }: MessageEvent<Theme>) => {
+      setTheme(data);
+    };
+
+    return () => {
+      channel.close();
+    };
+  }, []);
+
+  // Synchronize theme with CSS
+  useEffect(() => {
+    document.documentElement.setAttribute("data-theme", theme);
+  }, [theme]);
+
+  const handleThemeChange = (theme: Theme): void => {
+    setTheme(theme);
+
+    // Persist theme with a cookie
+    document.cookie = [
+      `${THEME_KEY}=${theme}`,
+      `max-age=${365 * 24 * 60 * 60}`, // 1 year
+      "secure",
+      "path=/",
+      "samesite=lax",
+    ].join("; ");
+
+    // Broadcast theme change to other tabs
+    channelRef.current?.postMessage(theme);
+  };
+
+  const toggleTheme = (): void => {
+    const isDark =
+      (theme === THEME.SYSTEM &&
+        matchMedia("(prefers-color-scheme: dark)").matches) ||
+      theme === THEME.DARK;
+
+    handleThemeChange(isDark ? THEME.LIGHT : THEME.DARK);
+  };
+
+  return (
+    <ThemeContext.Provider
+      value={{ theme, setTheme: handleThemeChange, toggleTheme }}
+    >
+      {children}
+    </ThemeContext.Provider>
+  );
+});
+
+ThemeProvider.displayName = "ThemeProvider";
