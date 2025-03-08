54d8040ba45efc9661b2eb7311fe8c5e837750c1
Johnny Gérard <john@jgerard.dev>
2025-03-08 06:27:37 +0100

    feat: add load-theme.ts

---
+++ src/load-theme.ts
@@ -0,0 +1,16 @@
+import { THEME_KEY } from "@/constants.js";
+import { type Theme, THEME } from "@/type/theme.js";
+import { cookies } from "next/headers.js";
+
+/**
+ * Server function to load user theme preference from a cookie.
+ * @returns User theme preference
+ * @see {@link https://nextjs.org/docs/app/api-reference/directives/use-server|use-server}
+ */
+export const loadTheme = async (): Promise<Theme> => {
+  "use server";
+  const cookieStore = await cookies();
+  const theme = cookieStore.get(THEME_KEY)?.value;
+
+  return theme === THEME.DARK || theme === THEME.LIGHT ? theme : THEME.SYSTEM;
+};
