f245c50f4ab25a28e8351ec6f2e4e085e739f8df
Johnny Gérard <john@jgerard.dev>
2025-03-08 08:02:08 +0100

    feat: add index.ts

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { THEME_KEY } from "@/constants.js";
+export { loadTheme } from "@/load-theme.js";
+export { ThemeProvider } from "@/theme-provider.js";
+export { type Theme, THEME } from "@/type/theme.js";
+export { useContextTheme } from "@/use-context-theme.js";
