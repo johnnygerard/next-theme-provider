8cfc0d61304430b3fc64f17fc444501e762d6ab3
Johnny Gérard <john@jgerard.dev>
2025-03-09 07:32:37 +0100

    refactor: rename use-context-theme.ts to use-theme-context.ts

--- src/index.ts
+++ src/index.ts
@@ -2,4 +2,4 @@ export { THEME_KEY } from "./constants.js";
 export { loadTheme } from "./load-theme.js";
 export { ThemeProvider } from "./theme-provider.js";
 export { type Theme, THEME } from "./type/theme.js";
-export { useContextTheme } from "./use-context-theme.js";
+export { useThemeContext } from "./use-theme-context.js";
--- src/use-theme-context.ts
+++ src/use-theme-context.ts
@@ -1,7 +1,7 @@
 import { useContext } from "react";
 import { ThemeContext } from "./theme-context.js";
 
-export const useContextTheme = () => {
+export const useThemeContext = () => {
   const context = useContext(ThemeContext);
   if (context) return context;
   throw new Error("Context provider not found");
